2baca42ff7f260c7c5982f959d2624c68b182909
angela <angelaguacaran@gmail.com>
2023-04-28 17:31:53 -0300

    aula-7

--- aulas/react/moz-todo-react/src/App.js
+++ aulas/react/moz-todo-react/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import Cabecalho from './Components/Cabecalho/Cabecalho';
+import Card from './Components/Card/Card';
+import fotos from './alura flix.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cabecalho />
+      <div id='cards'>
+        <Card
+          imagem={fotos}
+          texto="Lorem Ipsum is simply dummy text of the printing and"
+          titulo="Ola Mundo" />
+          <Card
+          imagem={fotos}
+          texto="Lorem Ipsum is simply dummy text of the printing and"
+          titulo="Ola Mundo" />
+      </div>
     </div>
   );
 }
